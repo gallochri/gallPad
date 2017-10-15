074efba656a3a5ac0bdc4ade8011b4ab8d1e1276
Christian Galeffi <chri@gallochri.com>
2017-10-15 18:26:34 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include "DigiKeyboard.h"
+
+// the setup routine runs once when you press reset:
+void setup() {
+    // initialize the digital pin as an output.
+    pinMode(0, OUTPUT); //LED on Model B
+    pinMode(1, OUTPUT); //LED on Model A  or Pro
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.println("Hello");
+    digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(1, HIGH);
+    delay(100);               // wait for a second
+    digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(1, LOW);
+    delay(200);               // wait for a second
+}
