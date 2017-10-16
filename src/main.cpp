3683ea173d05c13dc170b71edffea56998aba02f
Christian Galeffi <chri@gallochri.com>
2017-10-16 22:48:03 +0200

    Two button

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,8 @@
 #include "DigiKeyboard.h"
 
 #define LED 1
-#define BTN1 5
-#define BTN2 6
+#define BTN1 0
+#define BTN2 2
 
 int BTN1_state = 0;
 int BTN2_state = 0;
@@ -11,23 +11,21 @@ int BTN2_state = 0;
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(BTN1, INPUT);
-    pinMode(BTN2, INPUT);
     digitalWrite(BTN1,LOW);
+    pinMode(BTN2, INPUT);
     digitalWrite(BTN2,LOW);
 }
 
 void loop() {
     DigiKeyboard.sendKeyStroke(0);
     BTN1_state = digitalRead(BTN1);
-    BTN2_state = digitalRead(BTN2);
-
     if (BTN1_state == HIGH) {
-        DigiKeyboard.sendKeyPress(26);
         DigiKeyboard.println("pressed BTN1");
         digitalWrite(LED, HIGH);
     }
+
+    BTN2_state = digitalRead(BTN2);
     if (BTN2_state == HIGH) {
-        DigiKeyboard.sendKeyPress(27);
         DigiKeyboard.println("pressed BTN2");
         digitalWrite(LED, HIGH);
     }
